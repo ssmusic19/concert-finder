733a1e23978a7433eea76b2ae69f708eb8c27e08
ssmusic19 <ssmusic19@gmail.com>
2022-04-25 22:43:27 -0700

    init

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from app import app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b600001fefca09d5adceef79409c83f4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from app import routes
